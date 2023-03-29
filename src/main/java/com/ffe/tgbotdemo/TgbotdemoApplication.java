bad9789df6388e0b81d3c28040a600299ab37fa6
Garik <45121072+gtepanosyan@users.noreply.github.com>
2023-03-29 14:34:01 +0400

    Initial commit

---
+++ src/main/java/com/ffe/tgbotdemo/TgbotdemoApplication.java
@@ -0,0 +1,10 @@
+package com.ffe.tgbotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgbotdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgbotdemoApplication.class, args
---
+++ src/test/java/com/ffe/tgbotdemo/TgbotdemoApplicationTests.java
@@ -0,0 +1,10 @@
+package com.ffe.tgbotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgbotdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
